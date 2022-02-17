f2d4ee3382c7ddceed182b74e9084c637f13e665
Ryan Steed <rsteed11@gmail.com>
2022-02-17 18:20:23 -0500

    :arrow_upper_right: Revised CLI

--- scripts/policy_experiments.py
+++ scripts/policy_experiments.py
@@ -1,24 +1,37 @@
 from dp_policy.experiments import Experiment
+import click
 
 
-def run(experiments, **kwargs):
+@click.group(chain=True)
+def cli():
+    pass
+
+
+@cli.command('run')
+@click.argument('name')
+@click.option('--just-join', is_flag=True)
+def run(
+    name: str,
+    just_join: bool = False,
+    **kwargs
+):
+    print("--- Running", name, "---")
+    print("options:", kwargs)
+    experiment = Experiment.get_experiment(
+        name,
+        **kwargs
+    )
+    if not just_join:
+        experiment.run()
+    experiment.discrimination_join()
+
+
+@cli.command('run_all')
+@click.option('--just-join', is_flag=True)
+def run_all(experiments, **options):
     for name, options in experiments.items():
-        print("--- Running", name, "---")
-        experiment = Experiment.get_experiment(
-            name,
-            **kwargs
-        )
-        if not options.get('join_only'):
-            experiment.run()
-        experiment.discrimination_join()
+        run(name, **options)
 
 
 if __name__ == "__main__":
-    run({
-        # "hold_harmless": {},
-        # "post_processing": {},
-        # "moving_average_truth=average": {},
-        "thresholds": {},
-        "epsilon": {},
-        "budget": {}
-    })
+    cli()
